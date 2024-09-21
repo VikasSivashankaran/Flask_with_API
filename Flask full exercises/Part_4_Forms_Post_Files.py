9e9eac988186f0066178d5f110eaa09b0dbd05ab
VikasSivashankaran <vikasbmca@gmail.com>
2024-09-21 08:54:28 +0530

    new

--- Flask full exercises/Part_4_Forms_Post_Files.py
+++ Flask full exercises/Part_4_Forms_Post_Files.py
@@ -1,6 +1,8 @@
+#
+import pandas as pd
+import openpyxl
 
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response,Response, request, make_response
 
 app = Flask(__name__, template_folder='template4')
 
@@ -18,6 +20,30 @@ def index():
         else:
             return 'Failure'
 
+@app.route('/file_upload',methods=['POST'])
+def file_upload():
+    file = request.files['file']
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'or file.content_type=='application/vnd.ms-excel':
+        df=pd.read_excel(file)
+        return df.to_html()
+
+
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+    df = pd.read_excel(file)
+
+    # Convert DataFrame to CSV
+    csv_data = df.to_csv(index=False)
+
+    # Create response with appropriate headers
+    response = make_response(csv_data)
+    response.headers['Content-Disposition'] = 'attachment; filename=result.csv'
+    response.headers['Content-Type'] = 'text/csv'
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True )
